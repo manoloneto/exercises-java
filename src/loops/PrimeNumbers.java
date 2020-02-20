e8ac404fca6c2b243cb315ea1a86270f24646c59
Manoel Afonso <mano.afonso93@gmail.com>
2020-02-20 14:29:38 -0300

    Adding prime numbers exercise

---
+++ src/loops/PrimeNumbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Type a number: ");
+        int number = in.nextInt();
+
+        int count = 0;
+        for (int x = 1; x <= number; x++) {
+            count = 0;
+
+            for (int y = 1; y <= x; y++) {
+                if (x % y == 0) {
+                    count++;
+                }
+            }
+
+            if (count == 2) {
+                System.out.println(x);
+            }
+        }
+    }
+}
