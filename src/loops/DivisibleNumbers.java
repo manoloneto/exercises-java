b2df7eb4db6959da04717ed0140b2b540b3aa485
Manoel Afonso <mano.afonso93@gmail.com>
2020-02-20 14:41:21 -0300

    Adding divisible numbers exercise

---
+++ src/loops/DivisibleNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DivisibleNumbers {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Type the first number: ");
+        int first = in.nextInt();
+        System.out.print("Type the last number: ");
+        int last = in.nextInt();
+
+        System.out.print("Type the divisible number: ");
+        int div = in.nextInt();
+
+        for (int x = first; x <= last; x++) {
+            if (x % div == 0) {
+                System.out.println(x);
+            }
+        }
+    }
+}
